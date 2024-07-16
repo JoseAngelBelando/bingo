ac0c5f155434ef22b2734cbc6697a408d15b751e
joseAngel33 <163841769+joseAngel33@users.noreply.github.com>
2024-07-16 17:45:54 +0200

    +update

---
+++ src/js/dom.js
@@ -0,0 +1,20 @@
+// Obtener los elementos del DOM
+const userBoardElement = document.getElementById('user-board');
+const userTextElement = document.getElementById('user-text');
+const pcBoardElement = document.getElementById('pc-board');
+const pcTextElement = document.getElementById('pc-text');
+const bingoBoardElement = document.getElementById('bingo-board');
+const startButtonElement = document.getElementById('button-start');
+const restartButtonElement = document.getElementById('button-restart');
+const gameTextElement = document.getElementById('game-text');
+
+export {
+  userBoardElement,
+  userTextElement,
+  pcBoardElement,
+  pcTextElement,
+  bingoBoardElement,
+  startButtonElement,
+  restartButtonElement,
+  gameTextElement
+};
